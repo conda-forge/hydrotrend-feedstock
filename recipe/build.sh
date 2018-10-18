bba09d4c2e4993f35eddce1c92edd0ba0677d5be
mcflugen <mcflugen@gmail.com>
2018-10-18 09:57:56 -0600

    Clean up cmake options.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,9 @@
 
 mkdir _build && cd _build
 
-cmake  ..                                    \
-       -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX   \
-       -DCMAKE_PREFIX_PATH:PATH=$PREFIX      \
-       -DCMAKE_C_COMPILER=$CC                \
-       -DCMAKE_BUILD_TYPE=Release            \
-       -DDCMAKE_INCLUDE_PATH=$PREFIX/include \
-       -DCMAKE_LIBRARY_PATH=$PREFIX/lib
+cmake .. \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release
 
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install
